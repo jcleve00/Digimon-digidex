008cb3e66a04a2f0588d381c54a5d56ce85bf9bf
Grover2x <jcleve00@icloud.com>
2026-04-14 13:25:38 -0500

    Improve the grid to be more responsive and load more images with button click. Added digivice with buttons and screen.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+// Keeps track of digimon added to grid
+let currentIndex = 0;
+
+// Gets all digimon from server
 async function getAllDigimon() {
     const allDigimon = [];
     let page = 0;
@@ -24,75 +28,56 @@ async function getAllDigimon() {
 
     return allDigimon;
 }
-/**
- * @typedef {Object} DigimonData
- * @property {number} id - Unique ID
- * @property {string} name - Digimon name
- * @property {string} image - Image url
- */
-/**
- * Creates and renders a grid of Digimon cards.
- * @param {DigimonData[]} digimonList - Array of Digimon objects
- */
-function displayCards(digimonList) {
-    // Empty list to hold the cards
-    let cards = [];
-    // Iterate through digimonList and create the card elements 
-    digimonList.forEach(d => {
-        const cardDiv = document.createElement("div");
-        Object.assign(cardDiv.style, {
-            flex: '1 1 20%',
-            display: 'flex',
-            flexDirection: 'column',
-            background: 'grey',
-            width: '300px',
-            height: '500px',
-            margin: '10px auto',
-            borderRadius: '5px',
-            border: '2px solid black',
-            boxShadow: '0 0 10px'
-        });
 
+function displayCards(digimonList) {
+    offset = currentIndex + 12;
+    const cardGrid = document.getElementById('card-grid');
+
+    let slicedList = digimonList.slice(currentIndex, offset);
+
+    slicedList.forEach(d => {
+        const cardDiv = document.createElement("div");
+        cardDiv.className = 'cardDiv';
+        
         const cardName = document.createElement('h3');
         cardName.textContent = d.name;
-        Object.assign(cardName.style, {
-            textAlign: 'center',
-            color: 'black',
-            textShadow: '1px 1px 2px black'
-        });
-
+        
         const image = document.createElement('img');
+        image.className = 'digi-image';
         image.src = d.image;
         image.alt = `A image of ${d.name}, from Digimon.`;
-        Object.assign(image.style, {
-            width: '200px',
-            margin: '0px auto',
-            border: '1px solid darkGreen',
-            boxShadow: '0px 0px 10px white',
-        });
+
+        const cardId = document.createElement('p');
+        cardId.textContent = `#${d.id}`;
+        
         // Create the card and push it the cards list
         cardDiv.appendChild(cardName)
         cardDiv.appendChild(image);
-        cards.push(cardDiv);
+        cardDiv.appendChild(cardId);
+
+        cardDiv.addEventListener('click', () => {
+            window.location.href = `detail.html?id=${d.id}`;
+        });
+        
+        cardGrid.appendChild(cardDiv);
     });
-    // Create the cardGrid element then play the cards
-    const cardGrid = document.createElement('div');
-    Object.assign(cardGrid.style, {
-        display: 'flex',
-        flexFlow: 'row wrap',
-    });
-    cards.forEach(card => {
-        cardGrid.appendChild(card);
-    });
-    document.body.appendChild(cardGrid);
+    currentIndex += 12;
 }
+
 document.addEventListener("DOMContentLoaded", async () => {
     // Request the data from the api
     const digimon = await getAllDigimon();
     // Sort in aplhabetical order by name
-    let sortedDigimon = digimon.sort((a, b) => a.name.localeCompare(b.name));
-    console.log(digimon);
+    // let sortedDigimon = digimon.sort((a, b) => a.name.localeCompare(b.name));
     
-    displayCards(sortedDigimon);
+    // Display cards when page loads
+    displayCards(digimon);
+
+    // Load more cards when load button clicked
+    const loadMoreBtn = document.getElementById("load-more-button");
+    
+    loadMoreBtn.addEventListener('click', () => {
+        displayCards(digimon);
+    });    
 
 });
