577368e0873b702e22161aeede82799778cc8a9c
JTfromIT918 <justin.roudebush@atlasstudents.com>
2026-04-16 01:56:08 -0500

    Contact Form Finished, Needs Stylized

--- assets/js/script.js
+++ assets/js/script.js
@@ -81,3 +81,31 @@ document.addEventListener("DOMContentLoaded", async () => {
     });    
 
 });
+
+const form = document.getElementById("contactForm");
+const successMsg = document.getElementById("successMsg");
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const subject = document.getElementById("subject").value.trim();
+    const message = document.getElementById("message").value.trim();
+
+    if (!name || !email || !subject || !message) {
+        alert("Please fill out all fields.");
+        return;
+    }
+
+    // Fake submit (since no backend)
+    console.log({
+        name,
+        email,
+        subject,
+        message
+    });
+
+    successMsg.classList.remove("d-none");
+    form.reset();
+});
